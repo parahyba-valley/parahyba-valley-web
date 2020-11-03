d87cef2d0f75227a70249aa6c8c3fea8b794c0ba
Rafael nascimento <rafael.nascimento@redealumni.com>
2020-11-02 22:56:38 -0300

    remove slide rotation

--- src/components/slider/index.ts
+++ src/components/slider/index.ts
@@ -25,61 +25,99 @@ export default class Slider extends CustomComponent {
   }
 
   previous() {
-    if (this.currentSlideIndex - 1 < 0) {
-      this.currentSlideIndex = this.sliderData?.length;
-    }
-
     this.moveSlides(-1);
   }
 
   next() {
-    if (this.currentSlideIndex + 1 >= this.sliderData?.length) {
-      this.currentSlideIndex = -1;
-    }
-
     this.moveSlides(1);
   }
 
   moveSlides(times: number) {
+    if (!this.sliderData[this.currentSlideIndex + times]) {
+      return;
+    }
+
     if (this.sliderIntervalController) {
       clearInterval(this.sliderIntervalController);
       this.createSliderInterval();
     }
-    this.currentSlideIndex = this.currentSlideIndex + times;
 
+    this.currentSlideIndex = this.currentSlideIndex + times;
     this.moveIndicators(this.currentSlideIndex);
     this.slides.style.left = -100 * this.currentSlideIndex + '%';
+
+    this.checkControlsVisibility();
+  }
+
+  checkControlsVisibility() {
+    if (this.currentSlideIndex + 1 >= this.sliderData?.length) {
+      this.turnControlHidden('slider__control--right');
+    } else {
+      this.turnControlVisible('slider__control--right');
+    }
+
+    if (this.currentSlideIndex === 0) {
+      this.turnControlHidden('slider__control--left');
+    } else {
+      this.turnControlVisible('slider__control--left');
+    }
   }
 
   moveIndicators(activeIndex: number) {
     window.requestAnimationFrame(() => {
       const indicatorItens = this.indicators.children as HTMLCollectionOf<HTMLElement>;
+      const indicatorsOffsetLeft = this.indicators.offsetLeft;
       for (let i = 0; i < indicatorItens?.length; i++) {
         const element = indicatorItens[i];
         element.classList.remove('slider__control-indicators__item--last');
-        element.classList.remove('slider__control-indicators__item--second');
         element.classList.remove('slider__control-indicators__item--active');
-        element.classList.remove('slider__control-indicators__item--second');
         element.classList.remove('slider__control-indicators__item--last');
       };
 
-      indicatorItens[activeIndex - 1]?.classList.add('slider__control-indicators__item--last');
       indicatorItens[activeIndex]?.classList.add('slider__control-indicators__item--active');
-      indicatorItens[activeIndex + 1]?.classList.add('slider__control-indicators__item--last');
 
-      setTimeout(() => {
-        this.indicators.style.left = -(indicatorItens[activeIndex]?.offsetLeft - 40) + 'px';
-      }, 100);
+      if (
+        indicatorItens[activeIndex]?.offsetLeft >
+        (this.indicators.parentElement?.offsetWidth || 0) + (indicatorsOffsetLeft * -1)
+      ) {
+        setTimeout(() => {
+          this.indicators.style.left = -(indicatorItens[activeIndex]?.offsetLeft - 80) + 'px';
+        }, 100);
+
+      } else if (
+        indicatorItens[activeIndex]?.offsetLeft <
+        (indicatorsOffsetLeft * -1)
+      ) {
+        setTimeout(() => {
+          this.indicators.style.left = - indicatorItens[activeIndex]?.offsetLeft + 'px';
+        }, 100);
+      }
     });
   }
 
   initSlider() {
     this.moveIndicators(0);
+    this.checkControlsVisibility();
     this.createSliderInterval();
   }
 
+  turnControlVisible(elementClass: string) {
+    const control = this.component.elementRef?.getElementsByClassName(elementClass)[0];
+    if(control) {
+      control.classList.remove('slider__control--hidden');
+    }
+  }
+
+  turnControlHidden(elementClass: string) {
+    const control = this.component.elementRef?.getElementsByClassName(elementClass)[0];
+
+    if(control) {
+      control.classList.add('slider__control--hidden');
+    }
+  }
+
   createSliderInterval() {
-    this.sliderIntervalController = setInterval(() => {
+    this.sliderIntervalController = setTimeout(() => {
       this.next();
     }, 8000);
   }
