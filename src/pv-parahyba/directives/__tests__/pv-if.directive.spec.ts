7ec5988d652fd65cb0ad446ffdc37714e701df07
vitebo <vitebo@hotmail.com>
2021-02-20 17:49:15 -0300

    fix(pv-if): composition of logic gates

--- src/pv-parahyba/directives/__tests__/pv-if.directive.spec.ts
+++ src/pv-parahyba/directives/__tests__/pv-if.directive.spec.ts
@@ -23,31 +23,31 @@ function mount(options?: any): any {
 }
 
 describe('pv-if', () => {
-  describe('basic', () => {
-    test('show element when state is true', () => {
+  describe('A', () => {
+    test('displays the element on the screen', () => {
       const [template] = mount({
-        state: { show: true },
-        value: 'show',
+        state: { A: true },
+        value: 'A',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).toContainElement($target);
     });
 
-    test('hide element when state is false', () => {
+    test('removes the element on the screen', () => {
       const [template] = mount({
-        state: { show: false },
-        value: 'show',
+        state: { A: false },
+        value: 'A',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).not.toContainElement($target);
     });
   });
 
-  describe('dened', () => {
-    test('show element when state is false dened', () => {
+  describe('!A', () => {
+    test('displays the element on the screen', () => {
       const [template] = mount({
-        state: { show: false },
-        value: '!show',
+        state: { A: false },
+        value: '!A',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).toContainElement($target);
@@ -55,22 +55,22 @@ describe('pv-if', () => {
 
     test('hide element when state is true dened', () => {
       const [template] = mount({
-        state: { show: true },
-        value: '!show',
+        state: { A: true },
+        value: '!A',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).not.toContainElement($target);
     });
   });
 
-  describe('and', () => {
-    test('show element when and is true', () => {
+  describe('A && B', () => {
+    test('displays the element on the screen', () => {
       const [template] = mount({
         state: {
-          havePermission: true,
-          isLoaded: true,
+          A: true,
+          B: true,
         },
-        value: 'havePermission && isLoaded',
+        value: 'A && B',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).toContainElement($target);
@@ -79,48 +79,24 @@ describe('pv-if', () => {
     test('hide element when and an is false', () => {
       const [template] = mount({
         state: {
-          havePermission: false,
-          isLoaded: true,
+          A: false,
+          B: true,
         },
-        value: 'havePermission && isLoaded',
-      });
-      const $target = queryByTestId(template, 'target');
-      expect(template).not.toContainElement($target);
-    });
-
-    test('hide element when and all is false', () => {
-      const [template] = mount({
-        state: {
-          havePermission: false,
-          isLoaded: false,
-        },
-        value: 'havePermission && isLoaded',
+        value: 'A && B',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).not.toContainElement($target);
     });
   });
 
-  describe('or', () => {
-    test('show element when or an is true', () => {
+  describe('A || B', () => {
+    test('displays the element on the screen', () => {
       const [template] = mount({
         state: {
-          isOwner: true,
-          isPublic: false,
+          A: true,
+          B: false,
         },
-        value: 'isOwner || isPublic',
-      });
-      const $target = queryByTestId(template, 'target');
-      expect(template).toContainElement($target);
-    });
-
-    test('show element when or all is true', () => {
-      const [template] = mount({
-        state: {
-          isOwner: true,
-          isPublic: true,
-        },
-        value: 'isOwner || isPublic',
+        value: 'A || B',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).toContainElement($target);
@@ -129,160 +105,229 @@ describe('pv-if', () => {
     test('show element when or all is false', () => {
       const [template] = mount({
         state: {
-          isOwner: false,
-          isPublic: false,
+          A: false,
+          B: false,
         },
-        value: 'isOwner || isPublic',
+        value: 'A || B',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).not.toContainElement($target);
     });
   });
 
-  describe('and or', () => {
-    test('show element when all is true', () => {
+  describe('!A && B', () => {
+    test('displays the element on the screen', () => {
       const [template] = mount({
         state: {
-          hasData: true,
-          isPublic: true,
-          isAdmin: true,
+          A: false,
+          B: true,
         },
-        value: 'hasData && isPublic || isAdmin',
+        value: '!A && B',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).toContainElement($target);
     });
 
-    test('show element when a and is true', () => {
+    test('hide element when a and is true', () => {
       const [template] = mount({
         state: {
-          hasData: true,
-          isPublic: true,
-          isAdmin: false,
+          A: false,
+          B: false,
         },
-        value: 'hasData && isPublic || isAdmin',
-      });
-      const $target = queryByTestId(template, 'target');
-      expect(template).toContainElement($target);
-    });
-
-    test('show element when a and is true', () => {
-      const [template] = mount({
-        state: {
-          hasData: true,
-          isPublic: false,
-          isAdmin: true,
-        },
-        value: 'hasData && isPublic || isAdmin',
-      });
-      const $target = queryByTestId(template, 'target');
-      expect(template).toContainElement($target);
-    });
-
-    test('show element when a and is true', () => {
-      const [template] = mount({
-        state: {
-          hasData: false,
-          isPublic: true,
-          isAdmin: true,
-        },
-        value: 'hasData && isPublic || isAdmin',
-      });
-      const $target = queryByTestId(template, 'target');
-      expect(template).not.toContainElement($target);
-    });
-
-    test('show element when a and is true', () => {
-      const [template] = mount({
-        state: {
-          hasData: false,
-          isPublic: false,
-          isAdmin: false,
-        },
-        value: 'hasData && isPublic || isAdmin',
+        value: '!A && B',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).not.toContainElement($target);
     });
   });
 
-  describe('and dened', () => {
-    test('show element when a and is true', () => {
+  describe('A && !B', () => {
+    test('displays the element on the screen', () => {
       const [template] = mount({
         state: {
-          isPrivate: false,
-          isAdmin: true,
+          A: true,
+          B: false,
         },
-        value: 'isAdmin && !isPrivate',
+        value: 'A && !B',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).toContainElement($target);
     });
 
-    test('show element when a and is true', () => {
+    test('hide element when a and is true', () => {
       const [template] = mount({
         state: {
-          isPrivate: false,
-          isAdmin: true,
+          A: true,
+          B: true,
         },
-        value: '!isPrivate && isAdmin',
-      });
-      const $target = queryByTestId(template, 'target');
-      expect(template).toContainElement($target);
-    });
-  });
-
-  describe('or dened', () => {
-    test('show element when a and is true', () => {
-      const [template] = mount({
-        state: {
-          isPrivate: true,
-          isAdmin: false,
-        },
-        value: 'isAdmin || !isPrivate',
-      });
-      const $target = queryByTestId(template, 'target');
-      expect(template).not.toContainElement($target);
-    });
-
-    test('show element when a and is true', () => {
-      const [template] = mount({
-        state: {
-          isPrivate: true,
-          isAdmin: false,
-        },
-        value: '!isPrivate || isAdmin',
+        value: 'A && !B',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).not.toContainElement($target);
     });
   });
 
-  describe('and or dened', () => {
-    test('show element when all is true', () => {
+  describe('!A || B', () => {
+    test('displays the element on the screen', () => {
       const [template] = mount({
         state: {
-          hasData: true,
-          isPublic: true,
-          isAdmin: false,
+          A: false,
+          B: false,
         },
-        value: '!hasData && isPublic || isAdmin',
+        value: '!A || B',
+      });
+      const $target = queryByTestId(template, 'target');
+      expect(template).toContainElement($target);
+    });
+
+    test('hide element when a and is true', () => {
+      const [template] = mount({
+        state: {
+          A: true,
+          B: false,
+        },
+        value: '!A || B',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).not.toContainElement($target);
     });
+  });
 
-    test('show element when all is true', () => {
+  describe('A || !B', () => {
+    test('displays the element on the screen', () => {
       const [template] = mount({
         state: {
-          hasData: true,
-          isPublic: false,
-          isAdmin: true,
+          A: false,
+          B: false,
         },
-        value: 'hasData && !isPublic || isAdmin',
+        value: 'A || !B',
       });
       const $target = queryByTestId(template, 'target');
       expect(template).toContainElement($target);
+    });
+
+    test('hide element when a and is true', () => {
+      const [template] = mount({
+        state: {
+          A: false,
+          B: true,
+        },
+        value: 'A || !B',
+      });
+      const $target = queryByTestId(template, 'target');
+      expect(template).not.toContainElement($target);
+    });
+  });
+
+  describe('A && B || C', () => {
+    test('displays the element on the screen', () => {
+      const [template] = mount({
+        state: {
+          A: false,
+          B: true,
+          C: true,
+        },
+        value: 'A && B || C',
+      });
+      const $target = queryByTestId(template, 'target');
+      expect(template).toContainElement($target);
+    });
+
+    test('hide element when a and is true', () => {
+      const [template] = mount({
+        state: {
+          A: false,
+          B: true,
+          C: false,
+        },
+        value: 'A && B || C',
+      });
+      const $target = queryByTestId(template, 'target');
+      expect(template).not.toContainElement($target);
+    });
+  });
+
+  describe('A || B && C', () => {
+    test('displays the element on the screen', () => {
+      const [template] = mount({
+        state: {
+          A: false,
+          B: true,
+          C: true,
+        },
+        value: 'A || B && C',
+      });
+      const $target = queryByTestId(template, 'target');
+      expect(template).toContainElement($target);
+    });
+
+    test('hide element when a and is true', () => {
+      const [template] = mount({
+        state: {
+          A: false,
+          B: true,
+          C: false,
+        },
+        value: 'A || B && C',
+      });
+      const $target = queryByTestId(template, 'target');
+      expect(template).not.toContainElement($target);
+    });
+  });
+
+  describe('!A && B || C', () => {
+    test('displays the element on the screen', () => {
+      const [template] = mount({
+        state: {
+          A: false,
+          B: false,
+          C: true,
+        },
+        value: '!A && B || C',
+      });
+      const $target = queryByTestId(template, 'target');
+      expect(template).toContainElement($target);
+    });
+
+    test('hide element when all is true', () => {
+      const [template] = mount({
+        state: {
+          A: true,
+          B: true,
+          C: false,
+        },
+        value: '!A && B || C',
+      });
+      const $target = queryByTestId(template, 'target');
+      expect(template).not.toContainElement($target);
+    });
+  });
+
+  describe('A && !B || C', () => {
+    test('displays the element on the screen', () => {
+      const [template] = mount({
+        state: {
+          A: true,
+          B: false,
+          C: true,
+        },
+        value: 'A && !B || C',
+      });
+      const $target = queryByTestId(template, 'target');
+      expect(template).toContainElement($target);
+    });
+
+    test('hide element when all is true', () => {
+      const [template] = mount({
+        state: {
+          A: true,
+          B: true,
+          C: false,
+        },
+        value: 'A && !B || C',
+      });
+      const $target = queryByTestId(template, 'target');
+      expect(template).not.toContainElement($target);
     });
   });
 });
